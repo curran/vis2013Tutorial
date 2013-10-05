326934a6720dda0d0d14dd5711c99ed813a79196
curran <curran.kelleher@gmail.com>
2013-10-05 01:37:57 -0400

    Added interactive code example framework using CodeMirror and Inlet

--- loadSections.js
+++ loadSections.js
@@ -3,17 +3,41 @@
 // in `sections.json`, then injects the content
 // into the div with id "sections".
 
-function loadSection (name, callback) {
+(function () {
   var spacer = '<div class="spacer"></div>';
-  $.get('md/' + name + '.md', function (data) {
-    callback(null, spacer + marked(data));
-  });
-}
 
-function loadSections (names) {
-  async.map(names, loadSection, function (err, results) {
-    $('#sections').html(results.join(''));
-  });
-}
+  function ide (html) {
+    return [
+      '<iframe class="ide" src="ide.html#',
+      encodeURIComponent(html),
+      '"></iframe>'
+    ].join('');
+  }
 
-$.getJSON('sections.json', loadSections);
+  function loadExample (name, callback) {
+    $.get('examples/' + name + '.html', function (data) {
+      callback(null, data);
+    });
+  }
+
+  function loadSection (section, callback) {
+    $.get('md/' + section.name + '.md', function (data) {
+      var html = spacer + marked(data);
+      if (section.examples) {
+        async.map(section.examples, loadExample, function (err, results) {
+          callback(null, html + results.map(ide));
+        });
+      } else {
+        callback(null, html);
+      }
+    });
+  }
+
+  function loadSections (sections) {
+    async.map(sections, loadSection, function (err, results) {
+      $('#sections').html(results.join(''));
+    });
+  }
+
+  $.getJSON('sections.json', loadSections);
+}());
